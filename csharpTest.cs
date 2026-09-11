98af26132fe2622db10a6f86bb102cd7f12f8396
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2026-09-11 13:35:47 +0200

    Update csharpTest.cs

--- csharpTest.cs
+++ csharpTest.cs
@@ -1,18 +1,1 @@
-using System.Text;
-using Recite.Core.Csl;
-
-namespace Recite.Core.Citations;
-
-/// <summary>
-public sealed class CitationKeyScheme
-{
-    public string BaseKey(CslDocument doc)
-    {
-        ey = sb.ToString();
-        // Keep keys LaTeX-safe.
-        key = new string(key.Where(c => char.IsLetterOrDigit(c) || c is '-' or '_' or ':' or '.').ToArray());
-        return key.Length == 0 ? "ref" : key;
-    }
-}
-
-return key.Length == 0 ? "ref" : key;
+return key.Length ### ;
