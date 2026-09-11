5fa99a1946f5f002eafd64f50ba4b6693de87fbd
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2026-09-11 12:42:19 +0200

    Add default template to CitationKeyScheme
    Add default citation key template and constructor.

--- csharpTest.cs
+++ csharpTest.cs
@@ -1,6 +1,15 @@
 
 public sealed class CitationKeyScheme
 {
+
+    public const string Default = "[auth][year][shorttitle]";
+
+    public string Template { get; }
+
+    public CitationKeyScheme(string? template = null)
+        => Template = string.IsNullOrWhiteSpace(template) ? Default : template!;
+
+    
     /// ###
     public string BaseKey(CslDocument doc)
     {
