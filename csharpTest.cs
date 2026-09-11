db61693885f6869eb03f3346008335da6229395f
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2026-09-11 12:08:20 +0200

    Update csharpTest.cs

--- csharpTest.cs
+++ csharpTest.cs
@@ -1,22 +1,9 @@
-using System.Text;
-using Recite.Core.Csl;
-
-namespace Recite.Core.Citations;
-
 
 public sealed class CitationKeyScheme
 {
-    public const string Default = "[auth][year][shorttitle]";
-
-    public string Template { get; }
-
-    public CitationKeyScheme(string? template = null)
-        => Template = string.IsNullOrWhiteSpace(template) ? Default : template!;
-
-    /// <summary>Render the base (pre-collision) key for a document.</summary>
+    /// ###
     public string BaseKey(CslDocument doc)
     {
-        var sb = new StringBuilder();
+            /// ###
     }
-
 }
