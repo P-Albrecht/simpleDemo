8b3700314de46d5eda09e2f59dca65b31e07ec67
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2026-09-18 17:27:16 +0200

    Update cSharTest.cs

--- cSharTest.cs
+++ cSharTest.cs
@@ -1,28 +1,13 @@
-public class AuthenticateEndpoint : EndpointBaseAsync
-    .WithRequest<AuthenticateRequest>
-    .WithActionResult<AuthenticateResponse>
+[HttpPost("api/authenticate")]
+[SwaggerOperation(
+    Summary = "Authenticates a user",
+    Description = "Authenticates a user",
+    OperationId = "auth.authenticate",
+    Tags = new[] { "AuthEndpoints" })
+]
+public override async Task<ActionResult<AuthenticateResponse>> HandleAsync(AuthenticateRequest request,
+    CancellationToken cancellationToken = default)
 {
-    private readonly SignInManager<ApplicationUser> _signInManager;
-    private readonly ITokenClaimsService _tokenClaimsService;
- 
-    public AuthenticateEndpoint(SignInManager<ApplicationUser> signInManager,
-        ITokenClaimsService tokenClaimsService)
-    {
-        _signInManager = signInManager;
-        _tokenClaimsService = tokenClaimsService;
-    }
- 
-    [HttpPost("api/authenticate")]
-    [SwaggerOperation(
-        Summary = "Authenticates a user",
-        Description = "Authenticates a user",
-        OperationId = "auth.authenticate",
-        Tags = new[] { "AuthEndpoints" })
-    ]
-    public override async Task<ActionResult<AuthenticateResponse>> HandleAsync(AuthenticateRequest request,
-        CancellationToken cancellationToken = default)
-    {
-        var response = new AuthenticateResponse(request.CorrelationId());
-        return response;
-    }
+    var response = new AuthenticateResponse(request.CorrelationId());
+    return response;
 }
